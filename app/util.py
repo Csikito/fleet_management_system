7ba312bed5dbeb161a0bb1b98b85e187bb5870cd
Csikós Ronald <csikosronald@gmail.com>
2024-12-31 15:54:22 +0100

    not logged in user error handling

--- app/util.py
+++ app/util.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 
 def page_not_found(error):
+    if not current_user.is_authenticated:
+        return redirect(url_for('login_page.login'))
     return render_template('404.html'), 404
 
 def page_internal_server_error(error):
+    if not current_user.is_authenticated:
+        return redirect(url_for('login_page.login'))
     return render_template('500.html'), 500
 

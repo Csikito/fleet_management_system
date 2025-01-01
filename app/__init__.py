59705dcac813bd81f525f66ba4bd122463894eb7
Csikós Ronald <csikosronald@gmail.com>
2025-01-01 19:41:26 +0100

    set permissions and 403.html

--- app/__init__.py
+++ app/__init__.py
@@ -39,10 +39,11 @@ def create_app(flask_app, test=False):
 
         with app.app_context():
             from .routes import auth, dashboard
-            from .util import page_not_found, page_internal_server_error
+            from .util import page_not_found, page_internal_server_error , page_no_access
 
             app.register_blueprint(auth.login_page)
             app.register_blueprint(dashboard.admin_page)
+            app.register_error_handler(403, page_no_access)
             app.register_error_handler(404, page_not_found)
             app.register_error_handler(500, page_internal_server_error)
 
--- app/util.py
+++ app/util.py
@@ -1,6 +1,12 @@
 from flask import render_template, redirect, url_for
 from flask_login import current_user
 
+
+def page_no_access(error):
+    if not current_user.is_authenticated:
+        return redirect(url_for('login_page.login'))
+    return render_template('403.html'), 403
+
 def page_not_found(error):
     if not current_user.is_authenticated:
         return redirect(url_for('login_page.login'))
